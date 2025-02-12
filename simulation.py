032e1a2eb00290666d88c74e9b2e3a700477f616
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-02-12 15:34:13 -0300

    fixed state parsing

--- simulation.py
+++ simulation.py
@@ -1,14 +1,14 @@
 import gymnasium as gym
 from agent import Agent
 
-env_name = input("Enter the name of the environment [Taxi-v3, LunarLander-v3, CartPole-v1]: ")
-env = gym.make(env_name, render_mode="human").env
-MAX_EPISODES = 10000
+ENV_NAME = "Taxi-v3"
+MAX_EPISODES = 1000
 LEARNING_RATE = 0.001
 INITIAL_EPSILON = 1.0
 EPSILON_DECAY = 0.999
 FINAL_EPSILON = 0.01
 DISCOUNT_FACTOR = 0.95
+env = gym.make(ENV_NAME, render_mode="human").env
 agent = Agent(env, LEARNING_RATE, INITIAL_EPSILON, EPSILON_DECAY, FINAL_EPSILON, DISCOUNT_FACTOR)
 
 def log_experience(state, action, reward, next_state, done):
@@ -31,14 +31,15 @@ def log_q_table():
 done = False
 for i in range(MAX_EPISODES):
     print(f"Episode {i}")
-    state = env.reset()
+    state, info = env.reset()
     while not done:
         action = agent.select_action(state)
-        next_state, reward, done, truncated, info = env.step(action)
+        next_state, reward, terminated, truncated, info = env.step(action)
         agent.update(state, action, reward, done, next_state)
         log_experience(state, action, reward, next_state, done)
         log_q_table()
         env.render()
+        done = terminated or truncated
         state = next_state
     agent.decay_epsilon()
 
