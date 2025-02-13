bdf1535709534f9d05d1ab2ddf1890191ef8e7ff
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-02-13 20:04:52 -0300

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -18,42 +18,36 @@ env = RecordVideo(env, video_folder="videos", episode_trigger=lambda t: t % (MAX
 agent = Agent(env, LEARNING_RATE, INITIAL_EPSILON, EPSILON_DECAY, FINAL_EPSILON, DISCOUNT_FACTOR)
 
 def reset_log_files():
-    with open(f"logging/{EXPERIMENT_NAME}-experiences.txt", "w") as f:
-        f.write("")
     with open(f"logging/{EXPERIMENT_NAME}-q_table.txt", "w") as f:
         f.write("")
-
-def log_experience(state, action, reward, next_state, done):
-    experience = {
-        "state": state,
-        "action": action,
-        "reward": reward,
-        "next_state": next_state,
-        "done": done
-    }
-    with open(f"logging/{EXPERIMENT_NAME}-experiences.txt", "a") as f:
-        f.write(str(experience))
-        f.write("\n")
+    with open(f"logging/{EXPERIMENT_NAME}-episodes.txt", "w") as f:
+        f.write("")
 
 def log_q_table():
     with open(f"logging/{EXPERIMENT_NAME}-q_table.txt", "a") as f:
         f.write(str(agent.q_values))
         f.write("\n")
 
+def log_episodes(episode, accumulated_reward):
+    with open(f"logging/{EXPERIMENT_NAME}-episodes.txt", "a") as f:
+        f.write(f"Episode {episode}: {accumulated_reward}\n")
+
 reset_log_files()
 for i in range(MAX_EPISODES):
     print(f"Episode {i}")
     state, info = env.reset()
     done = False
+    accumulated_reward = 0
     while not done:
         action = agent.select_action(state)
         next_state, reward, terminated, truncated, info = env.step(action)
         done = terminated or truncated
         agent.update(state, action, reward, done, next_state)
-        log_experience(state, action, reward, next_state, done)
         log_q_table()
         env.render()
         state = next_state
+        accumulated_reward += reward
     agent.decay_epsilon()
+    log_episodes(i, accumulated_reward)
 
 env.close()
