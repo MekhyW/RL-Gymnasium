d148922be38bda758983a87f03cea0b5e43f3285
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-02-20 17:01:38 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+import numpy as np
 
 dfs = [pd.read_csv(f"logging/{file}") for file in os.listdir("logging") if file.endswith(".csv")]
 dfs_names = [file.split(".")[0] for file in os.listdir("logging") if file.endswith(".csv")]
@@ -30,3 +31,12 @@ plt.ylabel("Number of Steps")
 plt.savefig("plots/steps_plot.png", dpi=300, bbox_inches='tight')
 plt.show()
 plt.close()
+
+# Statistics for last 10% of episodes
+for df, name in zip(dfs, dfs_names):
+    last_10_percent = df.iloc[int(0.9 * len(df)):]
+    print(f"\nStatistics for {name} (last 10% of episodes):")
+    print(f"Average actions taken: {sum(last_10_percent['steps']) / len(last_10_percent):.2f}")
+    print(f"Standard deviation actions taken: {np.std(last_10_percent['steps']):.2f}")
+    print(f"Average rewards: {sum(last_10_percent['reward']) / len(last_10_percent):.2f}")
+    print(f"Standard deviation rewards: {np.std(last_10_percent['reward']):.2f}")
